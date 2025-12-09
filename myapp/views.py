7f835156969e8548991adf5a8391fb90eebd92cf
licheng <a0987500812@gmail.com>
2025-12-09 09:16:35 +0800

    B-branch bweb update1

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 #user A
+#user B
 from django.shortcuts import render, HttpResponse
 
 def test(request):
@@ -6,3 +7,5 @@ def test(request):
 
 def aweb(request):
     return HttpResponse("This is the aweb response.finish!")
+def bweb(request):
+    return HttpResponse("Hello, this is a bweb response.")
